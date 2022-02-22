70dc9c9368f51512c221ae699e5f9532d92a8fdd
Ryosuke Otsuka <ryosuke.otsuka@live.vu.edu.au>
2022-02-22 21:54:15 +1100

    New file

---
+++ Workshop 1.R
@@ -0,0 +1,34 @@
+150/3
+a <- 10
+a
+b <- "cool"
+b
+
+c <- c(3,4,7,9)
+c
+
+summary(c(3,4,7,9))
+
+
+install.packages("tidyverse")
+
+
+library(tidyverse)
+
+
+rm(b)
+
+
+
+
+
+data("mtcars")
+
+
+head(mtcars)
+
+summary(mtcars)
+
+glimpse(mtcars)
+
+
